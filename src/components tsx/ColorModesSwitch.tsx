81f605fa7341e5c6c85aad1b2c072569a63a386a
Meday98 <mohamedayad.work@gmail.com>
2025-03-25 01:17:15 +0100

    Build Search Input

--- src/components tsx/ColorModesSwitch.tsx
+++ src/components tsx/ColorModesSwitch.tsx
@@ -10,7 +10,7 @@ const ColorModesSwitch = () => {
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       />
-      <Text>Dark Mode</Text>
+      <Text whiteSpace="nowrap">Dark Mode</Text>
     </HStack>
   );
 };
